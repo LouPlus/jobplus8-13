1bbf138beb21e8708bafb64a6d756bf59fe5fe8b
duyunw <1627558962@qq.com>
2018-10-11 20:58:43 +0800

    fix config and models

---
+++ jobplus/config.py
@@ -0,0 +1,19 @@
+class BaseConfig():
+	SCRET_KEY ='makesure to set a very secret key'
+
+class DevelopmentConfig(BaseConfig):
+	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost:3306/jobplus?charset=utf-8'
+
+class ProductionConfig(BaseConfig):
+	pass
+
+class TestingConfig(BaseConfig):
+	pass
+
+
+configs = {
+	'development':DevelopmentConfig,
+	'production':ProductionConfig
+	'testing':TestingConfig
+}
