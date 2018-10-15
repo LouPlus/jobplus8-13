9bddcfb664ae6b52d9d6656bc4447fe51ef0cdc3
yuanshanghan <smjiao@outlook.com>
2018-10-15 22:42:00 +0800

    添加 # -*- coding: UTF-8 -*-

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from flask_wtf import FlaskForm
 from wtforms import (StringField,PasswordField,SubmitField,BooleanField)
 from wtforms.validators import Length,Email,EqualTo,Required
--- jobplus/models.py
+++ jobplus/models.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash,check_password_hash
