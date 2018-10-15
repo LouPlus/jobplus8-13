bd00d1f82e4e277e1b190176421d6e616e006ceb
huanglinglin <39321952+huanglinglin@users.noreply.github.com>
2018-10-14 22:49:01 -0500

    fix #22 add .gitignore (#30)
    * 修改一些bug 添加requ.txt
    
    * fix #22 add .gitignore

--- jobplus/app.py
+++ jobplus/app.py
@@ -6,17 +6,17 @@ from jobplus.config import configs
 from jobplus.models import db, User
 
 def register_extensions(app):
-	db.init_app(app)
-	Migrate(app, db)
+    db.init_app(app)
+    Migrate(app, db)
 
-	login_manager = LoginManager()
-	login_manager.init_app(app)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
-	@login_manager.user_loader
-	def user_loader(id):
-		return User.query.get(id)
-		
-	login_manager.login_view = 'front.login'
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
 
 def register_blueprints(app):
     from .handlers import front, job, company, admin, user, tests
@@ -29,27 +29,26 @@ def register_blueprints(app):
 
 def register_error_hanlers(app):
 
-	@app.errorhandler(404)
-	def not_ound(error):
-		return render_template('error/404.html'),404
+    @app.errorhandler(404)
+    def not_ound(error):
+        return render_template('error/404.html'),404
 
-	@app.errorhandler(500)
-	def server_error(error):
-		return render_template('error/500.html'),500
+    @app.errorhandler(500)
+    def server_error(error):
+        return render_template('error/500.html'),500
 
 def create_app(config):
 
-	app = Flask(__name__)
+    app = Flask(__name__)
 
-	if isinstance(config,dict):
-		app.config.update(config)
-	else:
-		app.config.from_object(configs.get(config,None))
+    if isinstance(config,dict):
+        app.config.update(config)
+    else:
+        app.config.from_object(configs.get(config,None))
 
-	db.init_app(app)
-	Migrate(app,db)
-	register_blueprints(app)
-	register_extensions(app)
-	register_error_hanlers(app)
-	return app
-
+    db.init_app(app)
+    Migrate(app,db)
+    register_blueprints(app)
+    register_extensions(app)
+    register_error_hanlers(app)
+    return app
