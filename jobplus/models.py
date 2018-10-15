974d1c49e6ab98d9b553be53c899738ddb6adf10
duyunw <1627558962@qq.com>
2018-10-15 16:39:25 +0800

    fix #13 add 添加数据库models.py文件备注

--- jobplus/models.py
+++ jobplus/models.py
@@ -35,18 +35,18 @@ class User(Base,UserMixin):
 	#用户名	
 	username = db.Column(db.String(32),unique=True, nullable=False)
 	#邮箱
-    email = db.Column(db.String(64),unique=True,index=True,nullable=False)
-	＃密码
-    _password = db.Column('password',db.String(256),nullable=False)
+	email = db.Column(db.String(64),unique=True,index=True,nullable=False)
+	#密码
+	_password = db.Column('password',db.String(256),nullable=False)
 	#角色
-    role = db.Column(db.SmallInteger,default=ROLE_USER)
+	role = db.Column(db.SmallInteger,default=ROLE_USER)
 	#简历 （连接地址）
-    resume = db.Column(db.String(64))
+	resume = db.Column(db.String(64))
 #	resume = db.relationship('Resume',uselist=False)
 	#关联 user与job的关联 （与上边的关联表对应） 
-    collect_jobs = db.relationship('Job',secondary=user_job)
+	collect_jobs = db.relationship('Job',secondary=user_job)
 	#
-    uoload_resume_url = db.Column(db.String(64))	
+	uoload_resume_url = db.Column(db.String(64))	
 
 	def __repr__(self):
 		return '<User:{}>'.format(self.name)
@@ -78,27 +78,27 @@ class Job(Base):
     #职位名称
 	name = db.Column(db.String(21))
 	#工资最低
-    salary_low = db.Column(db.Integer,nullable=False)
+	salary_low = db.Column(db.Integer,nullable=False)
 	#工资最高
-    salary_high = db.Column(db.Integer,nullable=False)
+	salary_high = db.Column(db.Integer,nullable=False)
 	#职位地址 在什么地方工作
-    location = db.Column(db.String(24))
+	location = db.Column(db.String(24))
     #职位标签
 	tags = db.Column(db.String(128))
 	#工作年限要求
-    experience_requirement = db.Column(db.String(32))
+	experience_requirement = db.Column(db.String(32))
 	#学历要求
-    degree_requirement = db.Column(db.String(32))
+	degree_requirement = db.Column(db.String(32))
 	#全职还是兼职
-    is_fulltime = db.Column(db.Boolean,default=True)
-    #岗位是否在招牌 
+	is_fulltime = db.Column(db.Boolean,default=True)
+   	#岗位是否在招牌 
 	is_open = db.Column(db.Boolean,default=True)
 	#公司的ID
-    company_id = db.Column(db.Integer,db.ForeignKey('company.id',ondelete='CASCADE'))
+	company_id = db.Column(db.Integer,db.ForeignKey('company.id',ondelete='CASCADE'))
 	#对应的公司
-    company = db.relationship('Company',uselist=False)
+	company = db.relationship('Company',uselist=False)
 	#查看次数
-    views_count = db.Column(db.Integer,default=0)
+	views_count = db.Column(db.Integer,default=0)
 
 	def __repr__(self):
 		return '<Job {}>'.format(self.name)
@@ -110,31 +110,29 @@ class Company(Base):
 	id = db.Column(db.Integer,primary_key=True)
     #企业名称
 	name = db.Column(db.String(64),nullable=False,index=True,unique=True)
-	#企业logo
-
-    logo = db.Column(db.String(64),nullable=True)
+	#企业lo
+	logo = db.Column(db.String(64),nullable=True)
 	#企业网址
-    site = db.Column(db.String(64),nullable=False)
+	site = db.Column(db.String(64),nullable=False)
 	#联系方式
-    contact =  db.Column(db.String(32),nullable=False)
+	contact =  db.Column(db.String(32),nullable=False)
 	#邮箱
-    email = db.Column(db.String(64),nullable=False)
+	email = db.Column(db.String(64),nullable=False)
 	#地理位置
-    location = db.Column(db.String(24),nullable=False)
+	location = db.Column(db.String(24),nullable=False)
     #一句话描述
 	describe = db.Column(db.String(128),nullable=False)
 	#详细介绍
-    about = db.Column(db.String(1024))
+	about = db.Column(db.String(1024))
 	#公司标签
-    tags = db.Column(db.String(128))
+	tags = db.Column(db.String(128))
 	#公司技术栈
-    stack = db.Column(db.String(128))
+	stack = db.Column(db.String(128))
     
     #公司团队
 	team_introduction = db.Column(db.String(256))
 	#公司福利
-    welfares = db.Column(db.String(256))
-
+	welfares = db.Column(db.String(256))
 	user_id = db.Column(db.Integer,db.ForeignKey('user.id',ondelete='SET NULL'))
 	user = db.relationship('User',uselist=False,backref=db.backref('company',uselist=False))
 
@@ -153,12 +151,10 @@ class Dilivery(Base):
 	STATUS_ACCEPT = 3
 
 	id = db.Column(db.Integer,primary_key=True)
-
-    job_id = db.Column(db.Integer,db.ForeignKey('job.id',ondelete='SET NULL'))
+	job_id = db.Column(db.Integer,db.ForeignKey('job.id',ondelete='SET NULL'))
 	user_id = db.Column(db.Integer,db.ForeignKey('user.id',ondelete='SET NULL'))
     
     #状态信息
 	status = db.Column(db.SmallInteger,default=STATUS_WAITING)
 	#企业的回应
-    response = db.Column(db.String(256))
-
+	response = db.Column(db.String(256))
