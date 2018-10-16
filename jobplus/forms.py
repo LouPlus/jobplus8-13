a29b00e9462067f268e8ca843deb17332b89203a
huanglinglin <39321952+huanglinglin@users.noreply.github.com>
2018-10-16 04:37:18 -0500

    fix LouPlus#38 添加注册登录 flash (#39)
    *  fix #33 添加注册登录页面
    
    * fix #35 实现macros.html 表单模块化
    
    * fix #38 实现登录注册页面并有flash提示
    
    * fix #38 添加注册登录 flash

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 from flask_wtf import FlaskForm
-from wtforms import (StringField,PasswordField,SubmitField,BooleanField)
+from wtforms import (StringField,PasswordField,SubmitField,BooleanField,ValidationError)
 from wtforms.validators import Length,Email,EqualTo,Required
 from jobplus.models import db,User
 
@@ -16,6 +16,7 @@ class UserregisterForm(FlaskForm):
         user.username = self.username.data
         user.email = self.email.data
         user.password = self.password.data
+        db.session.add(user)
         db.session.commit()
         return user
 
@@ -33,6 +34,24 @@ class CompanyregisterForm(FlaskForm):
     password = StringField('密码',validators=[Required(),Length(6,24,message='密码在6-24之间')])
     repeat_password = PasswordField('重复密码',validators=[Required(),EqualTo('password')])
     submit = SubmitField('提交')
+
+    def validate_username(self,field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经存在')
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经存在')
+
+    def create_user(self):
+        user = User()
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password =self.password.data
+        db.session.add(user)
+        db.session.commit()
+        return user
+
 
 
 class LoginForm(FlaskForm):
