17e5a886387ec6049df6dd020cef2266db4e12b7
huanglinglin <984531211@qq.com>
2018-10-17 20:40:46 +0800

    fix #18 实现个人配置页面

--- jobplus/handlers/user.py
+++ jobplus/handlers/user.py
@@ -1,11 +1,17 @@
 from flask import Blueprint
 from flask import render_template
 from jobplus.decorators import user_required
+from jobplus.forms import UserForm
 
-user = Blueprint('user', __name__,url_prefix='/user')
+user = Blueprint('user', __name__)
 
 @user.route('/user')
 @user_required
 def index():
-    return render_template('user/index.html')
+    form = UserForm()
+    if form.validate_on_submit():
+        form.user_data()
+        flash('填写数据成功，跳转到首页!','success')
+        return redirect(url_for('front.index'))
+    return render_template('user/index.html',form=form)
 
